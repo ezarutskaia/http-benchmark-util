35976cbb36a23b98aba712dba18223ca41612d9a
Elena <ezarutskaia@gmail.com>
2024-11-15 16:35:54 +0400

    task-3

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"net/http"
 	"io"
 	"os"
+	"time"
 )
+
+type Request struct {
+	TimeStart time.Time
+	TimeEnd time.Time
+	TimeDuration time.Duration
+	DataVolume int
+}
+
+var requests []Request
 
 func URLresponse(url string, n int)  {
 	//var resp *http.Response
 	for i := 1; i <= n; i++ {
+		start := time.Now()
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Println(err)
@@ -21,10 +32,25 @@ func URLresponse(url string, n int)  {
 			fmt.Println(err)
 			continue
 		}
-		fmt.Printf("%s\n", body)
+		end := time.Now()
+		duration := end.Sub(start)
+		req := Request{start, end, duration, len(body)}
+		requests = append(requests, req)
 		func () {
 			defer resp.Body.Close()
 		} ()
+	}
+	stdOut(requests)
+}
+
+func stdOut(sliceReq []Request) {
+	timeFormat := "2006-01-02 15:04:05.999999999"
+	fmt.Println("TimeStart 		     TimeEnd 			 TimeDuration  	DataVolume")
+    fmt.Println("====================================================================================")
+	for _, req := range sliceReq {
+		startFormat := req.TimeStart.Format(timeFormat)
+		endFormat := req.TimeEnd.Format(timeFormat)
+		fmt.Printf("%-28v %-28v %14v %10v\n", startFormat, endFormat, req.TimeDuration, req.DataVolume)
 	}
 }
 
